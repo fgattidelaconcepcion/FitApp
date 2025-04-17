ff3bcabec66ec7c4425bdd750844374dee7d2c9a
¨FrancoGatti¨ <fgattidelaconcepcion@gmail.com>
2025-04-16 22:08:12 -0300

    Se cambiaron global colors

--- app/global/colors.js
+++ app/global/colors.js
@@ -1,13 +1,13 @@
 export const colors = {
     text: {
-        primary: '#000000', // Blanco
+        primary: '#000000', // Negro
         secondary: '#FFFFFF', // Blanco
         light: '#FFFFFF', // Blanco
     },
     error: '#D32F2F', // Rojo
     success: '#388E3C', // Verde
     info: '#1976D2', // Azul
-    primary: '#000000F', // Blanco
-    secondary: '#000000', // Blanco
-    background: '#FFFFFF', // Negro
+    primary: '#000000F', // Rojo
+    secondary: '#000000', // Negro
+    background: '#FFFFFF', //Blanco
 };
